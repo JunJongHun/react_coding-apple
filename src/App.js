f1f14254adfe4d80d644414ae2eed2c733212c4f
JongHun <minn4072@gmail.com>
2022-04-29 23:46:22 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import { useState } from "react";
 function App() {
+  let [logo, setLogo] = useState("React Start");
+  let [title, setTitle] = useState(["frist", "second", "third"]);
+  let [date, setDate] = useState(new Date().toLocaleString());
   return (
     <div className="App">
       <div>
-        <h4 className="black-nav">블로그</h4>
+        <h4 className="black-nav">{logo}</h4>
+      </div>
+      <div className="list">
+        <h4>{title[0]}</h4>
+        <p>{date}</p>
+      </div>
+      <div className="list">
+        <h4>{title[1]}</h4>
+        <p>{date}</p>
+      </div>
+      <div className="list">
+        <h4>{title[2]}</h4>
+        <p>{date}</p>
       </div>
     </div>
   );
