4c895710bc34acedc6b49452e27fc4300d1635b9
JunJongHun <90402926+JunJongHun@users.noreply.github.com>
2022-05-02 01:56:59 +0900

    state

--- src/App.js
+++ src/App.js
@@ -4,14 +4,33 @@ function App() {
   let [logo, setLogo] = useState("React Start");
   let [title, setTitle] = useState(["frist", "second", "third"]);
   let [date, setDate] = useState(new Date().toLocaleString());
+  let [like, setLike] = useState(0);
+  let [name, nameTitle] = useState("여자코트추천");
+
   return (
     <div className="App">
       <div>
         <h4 className="black-nav">{logo}</h4>
       </div>
       <div className="list">
-        <h4>{title[0]}</h4>
+        <h4>
+          {title[0]}
+          <span onClick={() => setLike((prev) => prev + 1)}> 👍</span>
+          {like}
+        </h4>
+
         <p>{date}</p>
+        <button
+          onClick={() => {
+            setTitle((prev) => {
+              let copy = [...prev];
+              copy[0] = name;
+              return copy;
+            });
+          }}
+        >
+          제목수정
+        </button>
       </div>
       <div className="list">
         <h4>{title[1]}</h4>
