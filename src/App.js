13fd617ed2b906a28d7e02263e26d1c966092d83
JongHun <minn4072@gmail.com>
2022-05-05 18:50:50 +0900

    props update

--- src/App.js
+++ src/App.js
@@ -4,71 +4,75 @@ function App() {
   let [logo, setLogo] = useState("React Start");
   let [title, setTitle] = useState(["frist", "second", "aaa"]);
   let [date, setDate] = useState(new Date().toLocaleString());
-  let [like, setLike] = useState(0);
+  let [like, setLike] = useState([0, 0, 0]);
   let [name, nameTitle] = useState("여자코트추천");
   let [modal, setModal] = useState(false);
+
+  const showModal = () => {
+    // setModal(!modal);
+    setModal((prev) => {
+      return !prev;
+    });
+  };
+
+  const countUpLike = (index) => {
+    setLike((prev) => {
+      let copy = [...like];
+      copy[index] += 1;
+      return copy;
+    });
+  };
+
+  const sortTitle = () => {
+    let copy = [...title];
+    copy.sort();
+    setTitle(copy);
+  };
+
+  const changeTitle = () => {
+    let copy = [...title];
+    copy[0] = name;
+    setTitle(copy);
+  };
+
   return (
     <div className="App">
       <div>
         <h4 className="black-nav">{logo}</h4>
       </div>
-      <div className="list">
-        <h4
-          onClick={() => {
-            setModal((prev) => {
-              return !prev;
-            });
-          }}
-        >
-          {title[0]}
-        </h4>
-        <span onClick={() => setLike((prev) => prev + 1)}> 👍</span>
-        {like}
 
-        <p>{date}</p>
-        <button
-          onClick={() => {
-            setTitle((prev) => {
-              let copy = [...prev];
-              copy[0] = name;
-              return copy;
-            });
-          }}
-        >
-          제목수정
-        </button>
-      </div>
-      <div className="list">
-        <h4>{title[1]}</h4>
-        <p>{date}</p>
-      </div>
-      <div className="list">
-        <h4>{title[2]}</h4>
-        <p>{date}</p>
-      </div>
-      <button
-        onClick={() => {
-          setTitle((prev) => {
-            let copy = [...prev];
-            copy.sort();
-            return copy;
-          });
-        }}
-      >
-        제목 순서 재배치
-      </button>
+      {title.map((value, index) => {
+        return (
+          <div className="list">
+            <h4 onClick={showModal}>{value}</h4>
+            <span
+              onClick={() => {
+                countUpLike(index);
+              }}
+            >
+              👍
+            </span>
+            {like[index]}
 
-      {modal ? <Modal></Modal> : null}
+            <p>{date}</p>
+          </div>
+        );
+      })}
+
+      <button onClick={sortTitle}>제목 순서 재배치</button>
+
+      {modal ? <Modal title={title} changeTitle={changeTitle}></Modal> : null}
     </div>
   );
 }
 
-function Modal() {
+function Modal(props) {
   return (
     <div className="modal">
-      <h4>title</h4>
+      <h4>{props.title[0]}</h4>
       <p>day</p>
       <p>description</p>
+      <button onClick={props.changeTitle}>제목 수정</button>
     </div>
   );
 }
